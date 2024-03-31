3bcd0a3c4e070e06a3de0f43043630e9fa17e2ea
eeshaanpahuja <165544933+eeshaanpahuja@users.noreply.github.com>
2024-03-31 12:53:27 +0530

    Create forloop.py

---
+++ forloop.py
@@ -0,0 +1,45 @@
+a = 10000
+print("Hello", a-30)
+a=a-30
+print(a)
+b = 100000
+print("Hello", b-30)
+b=b-30
+print(b)
+c = 100000
+print("Hello", c-30)
+c=c-30
+print(c)
+d = 1000000
+print("Hello", d-30)
+d=d-30
+print(d)
+e = 10000000
+print("Hello", e-30)
+e=e-30
+print(e)
+f = 100000000
+print("Hello", f-30)
+f=f-30
+print(f)
+g = 1000000000
+print("Hello", g-30)
+g=g-30
+print(g)
+h = 10000000000
+print("Hello", h-30)
+h=h-30
+print(h)
+i = 100000000000
+print("Hello", i-30)
+i=i-30
+print(i)
+j = 1000000000000
+print("Hello", j-30)
+j=j-30
+print(j)
+k = 10000000000000
+print("Hello", k-30)
+k=k-30
+print(k)
+
